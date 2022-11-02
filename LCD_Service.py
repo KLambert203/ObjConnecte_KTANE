4889361bea4255afd6e45c3e246714e5bfe8f673
xaviergs27 <xaviergsylvain@gmail.com>
2022-11-02 14:21:46 -0400

    Creation classe LCD_Service

---
+++ LCD_Service.py
@@ -0,0 +1,57 @@
+from keypad import Keypad
+from PCF8574 import PCF8574_GPIO
+from ADAFruit_LCD1602 import Adafruit_CharLCD
+from Timer import timer
+
+class LCD:
+    def __init__(self):
+        self.lcd = self.init_chips()
+        self.keypad = self.init_keypad()
+
+    def init_chips(self):
+        PCF8574_address = 0x27  # I2C address of the PCF8574 chip.
+        PCF8574A_address = 0x3F
+
+        try:
+            mcp = PCF8574_GPIO(PCF8574_address)
+        except:
+            try:
+                mcp = PCF8574_GPIO(PCF8574A_address)
+            except:
+                print('I2C Address Error !')
+                exit(1)
+
+        return Adafruit_CharLCD(pin_rs=0, pin_e=2, pins_db=[4, 5, 6, 7], GPIO=mcp), mcp
+
+    def init_keypad(self):
+        rows = 4  # number of rows of the Keypad
+        cols = 4  # number of columns of the Keypad
+        keys = ['1', '2', '3', 'A',  # key code
+                '4', '5', '6', 'B',
+                '7', '8', '9', 'C',
+                '*', '0', '#', 'D']
+        rows_pins = [36, 38, 40, 29]
+        cols_pins = [31, 33, 35, 37]
+        return Keypad(keys, rows_pins, cols_pins, rows, cols)
+
+
+    def loop(self):
+        self.keypad.setDebounceTime(50)
+        self.lcd[1].output(3, 1)
+        self.lcd[0].begin(16, 2)
+        string_array = []
+        while True:
+            key = self.keypad.getKey()
+            if key != self.keypad.NULL:
+                self.lcd[0].setCursor(0, 0)
+                string_array.append(str(key))
+                string = ""
+                for a in string_array:
+                    string += str(a)
+                self.lcd[0].clear()
+                self.lcd[0].message(string + '\n')
+
+    def clear(self):
+        self.lcd[0].clear()
+
+
--- main.py
+++ main.py
@@ -6,63 +6,16 @@
 # modification: 2018/08/03
 ########################################################################
 import RPi.GPIO as GPIO
-from keypad import Keypad # import module Keypad
-from PCF8574 import PCF8574_GPIO
-from ADAFruit_LCD1602 import Adafruit_CharLCD
+from LCD_Service import LCD
 
+lcd_service = LCD()
 
-PCF8574_address = 0x27  # I2C address of the PCF8574 chip.
-PCF8574A_address = 0x3F
-
-try:
-    mcp = PCF8574_GPIO(PCF8574_address)
-except:
-    try:
-        mcp = PCF8574_GPIO(PCF8574A_address)
-    except:
-        print('I2C Address Error !')
-        exit(1)
-
-lcd = Adafruit_CharLCD(pin_rs=0, pin_e=2, pins_db=[4, 5, 6, 7], GPIO=mcp)
-
-ROWS = 4  # number of rows of the Keypad
-COLS = 4  # number of columns of the Keypad
-keys = ['1', '2', '3', 'A',  # key code
-        '4', '5', '6', 'B',
-        '7', '8', '9', 'C',
-        '*', '0', '#', 'D']
-rowsPins = [36, 38, 40, 29]
-colsPins = [31, 33, 35, 37]
-
-
-
-
-def loop():
-    keypad = Keypad(keys, rowsPins, colsPins, ROWS, COLS)  # creat Keypad object
-    keypad.setDebounceTime(50)  # set the debounce time
-    mcp.output(3, 1)  # turn on LCD backlight
-    lcd.begin(16, 2)  # set number of LCD lines and columns
-    stringArray = []
-        # lcd.clear()
-    string = ""
-    while (True):
-        key = keypad.getKey()  # obtain the state of keys
-        if (key != keypad.NULL):  # if there is key pressed, print its key code.
-            lcd.setCursor(0, 0)  # set cursor position
-            stringArray.append(str(key))
-            string = ""
-            for a in stringArray:
-                string += str(a)
-            lcd.clear()
-            lcd.message(string + '\n')  # display CPU temperature
-
-
-
-if __name__ == '__main__':  # Program start from here
+if __name__ == '__main__':
     print("Program is starting ... ")
     try:
-        loop()
+        lcd_service.loop()
+
     except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, exit the program.
         GPIO.cleanup()
     finally:
-        lcd.clear()
+        lcd_service.clear()
