6e23661a43d8cf0f6256b40043590170e62a86cb
KLambert203 <keven.lambert203@gmail.com>
2022-11-09 14:15:54 -0500

    ajout de fonctions dans le manager

--- GameManager.py
+++ GameManager.py
@@ -1,4 +1,5 @@
 import paho.mqtt.client as paho
+import paho.mqtt.subscribe as subscribe
 import time
 
 
@@ -7,8 +8,24 @@ class GameManager:
         self.broker_ip = broker_ip
         self.listener_port = listener_port
         self.game_modules = game_modules
+        self.mqttc = paho.Client(client_id="manager", clean_session=False)
+
+    def start_game(self):
+        self.connect_to_broker()
+        self.mqttc.publish("game/lives", 3, 1, True)
 
     def connect_to_broker(self):
-        mqttc = paho.Client(client_id="pub_test", clean_session=False)
-        mqttc.connect(self.broker_ip, port=self.listener_port, keepalive=60, bind_address="")
+        self.mqttc.connect(self.broker_ip, port=self.listener_port, keepalive=60, bind_address="")
 
+    def activate_module(self, module):
+        path = "modules/" + module + "active"
+        self.mqttc.publish(path, True, 1, True)
+
+    def deactivate_module(self, module):
+        path = "modules/" + module + "active"
+        self.mqttc.publish(path, False, 1, True)
+
+    def decrease_number_of_lives(self):
+        current_lives = subscribe.simple("game/lives", hostname=self.broker_ip)
+        updated_lives = current_lives - 1
+        self.mqttc.publish("game/lives", updated_lives, 1, True)
--- main.py
+++ main.py
@@ -5,26 +5,14 @@
 # Author      : freenove
 # modification: 2018/08/03
 ########################################################################
-import RPi.GPIO as GPIO
-from keypad import Keypad # import module Keypad
+import GameManager
 
-ROWS = 4  # number of rows of the Keypad
-COLS = 4  # number of columns of the Keypad
-keys = ['1', '2', '3', 'A',  # key code
-        '4', '5', '6', 'B',
-        '7', '8', '9', 'C',
-        '*', '0', '#', 'D']
-rowsPins = [36, 38, 40, 29]
-colsPins = [31, 33, 35, 37]
-
+manager = GameManager.GameManager("10.4.1.43", 1883, [])
 
 def loop():
-    keypad = Keypad(keys, rowsPins, colsPins, ROWS, COLS)  # creat Keypad object
-    keypad.setDebounceTime(50)  # set the debounce time
-    while (True):
-        key = keypad.getKey()  # obtain the state of keys
-        if (key != keypad.NULL):  # if there is key pressed, print its key code.
-            print("You Pressed Key : %c " % (key))
+    manager.start_game()
+    while True:
+        print("hello world \r")
 
 
 if __name__ == '__main__':  # Program start from here
@@ -32,4 +20,4 @@ if __name__ == '__main__':  # Program start from here
     try:
         loop()
     except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, exit the program.
-        GPIO.cleanup()
+        print("Game has ended")
