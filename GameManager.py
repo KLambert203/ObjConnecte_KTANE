9abd31a0e5a9b8606bfc8b32b630f2abe4507ca4
KLambert203 <keven.lambert203@gmail.com>
2022-11-23 14:36:06 -0500

    ajout de fonctions de gestion de partie

--- GameManager.py
+++ GameManager.py
@@ -24,12 +24,32 @@ class GameManager:
             self.mqttc.publish(path, False, 1, True)
 
     def end_game(self):
+        self.connect_to_broker()
         self.mqttc.publish("game/lives", 3, 1, True)
         self.mqttc.publish("game/isStarted", False, 1, True)
         for module in self.game_modules:
             path = "game/modules/" + module + "/isActive"
             self.mqttc.publish(path, False, 1, True)
+        exit(0)
 
+    def game_over(self):
+        print("You have lost! Game Over!")
+        self.end_game()
+
+    def manage_game(self):
+        while True:
+            current_module = self.game_modules[self.module_index]
+            print("Checking number of lives")
+            if int(subscribe.simple("game/lives", hostname=self.broker_ip).payload) <= 0:
+                print("Out of lives")
+            print("Checking timer")
+            if int(subscribe.simple("game/timer", hostname=self.broker_ip).payload) <= 0:
+                print("Out of time")
+            print("Checking if current module is in failure")
+            self.is_module_in_failure(current_module)
+            print("Checking if current module has been won")
+            self.has_module_been_won(current_module)
+            time.sleep(0.5)
 
     def connect_to_broker(self):
         self.mqttc.connect(self.broker_ip, port=self.listener_port, keepalive=60, bind_address="")
@@ -37,8 +57,10 @@ class GameManager:
     def activate_module(self, module):
         path1 = "game/modules/" + module + "/isActive"
         path2 = "game/modules/" + module + "/Fail"
+        path3 = "game/modules/" + module + "/Success"
         self.mqttc.publish(path1, True, 1, True)
         self.mqttc.publish(path2, False, 1, True)
+        self.mqttc.publish(path3, False, 1, True)
 
     def deactivate_module(self, module):
         path1 = "game/modules/" + module + "/isActive"
@@ -51,14 +73,32 @@ class GameManager:
         updated_lives = current_lives - 1
         self.mqttc.publish("game/lives", updated_lives, 1, True)
 
-    def win_module(self):
+    def has_module_been_won(self, module):
+        module_status = str(subscribe.simple("game/modules/" + module + "/Success", hostname=self.broker_ip).payload)
+        print(module_status)
+        if module_status == "b'True'":
+            self.win_module(module)
+            path = "game/modules/" + module + "/Fail"
+            self.mqttc.publish(path, False, 1, True)
+
+    def is_module_in_failure(self, module):
+        path = "game/modules/" + module + "/Fail"
+        module_status = str(subscribe.simple(path, hostname=self.broker_ip).payload)
+        print(module_status)
+        if module_status == "b'True'":
+            self.decrease_number_of_lives()
+            path = "game/modules/" + module + "/Fail"
+            self.mqttc.publish(path, False, 1, True)
+
+    def win_module(self, module):
         if self.module_index + 1 > len(self.game_modules):
             self.win_game()
-        won_module = "game/modules/" + self.game_modules[self.module_index] + "/isActive"
+            return 0
+        won_module = "game/modules/" + module + "/isActive"
         new_module = "game/modules/" + self.game_modules[self.module_index + 1] + "/isActive"
         self.module_index += 1
-        self.mqttc.publish(won_module, False, 1, True)
-        self.mqttc.publish(new_module, True, 1, True)
+        self.deactivate_module(module)
+        self.activate_module(module)
 
     def win_game(self):
         self.mqttc.publish("game/hasBeenWon", True, 2, True)
--- manage_game.py
+++ manage_game.py
@@ -6,6 +6,7 @@
 # modification: 2018/08/03
 ########################################################################
 import GameManager
+import threading
 
 manager = GameManager.GameManager("10.4.1.43", 1883, [])
 manager.game_modules = ["Simon", "Keypad"]
@@ -15,7 +16,7 @@ def start_game(input_choice):
         print("Game has started. Timer set to 3 minutes!")
         print("Good luck!")
         manager.start_game()
-        #manager.manage_game()
+        manager.manage_game()
     elif input_choice == "E":
         print("Game has ended. Thanks for playing!")
         manager.end_game()
@@ -36,5 +37,6 @@ if __name__ == '__main__':  # Program start from here
     print("Program is starting ... ")
     try:
         loop()
+        manager.end_game()
     except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, exit the program.
         print("Game has ended")
